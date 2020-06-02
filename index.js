102f2388765d96d545652954b4f91006e81a8239
bryant00 <bryantpatton@gmail.com>
2020-06-02 14:35:32 -0700

    tests

--- index.js
+++ index.js
@@ -20,22 +20,42 @@ express()
       resave: true,
     })
   )
-  // <form action="/connect/linkedin" method="POST">
   .use(parser.urlencoded({ extended: true }))
   .use(grant(config))
   .get("/", (req, res) => {
     res.writeHead(200, { "content-type": "text/html" });
     res.end(`
       <!DOCTYPE html>
-      <form action="/connect/linkedin?callback=${callback}" method="POST">
-        <p>Grant read/write access to:</p>
-        <button>Login</button>
+      <p>Grant read/write access to:</p>
+      <form action="/connect/linkedin/" method="POST">
+
+        <button>Login Here</button>
+      </form>
+      <form action="/connect/linkedin/ext" method="POST">
+        <button>EXT Login Here</button>
       </form>
     `);
   })
   .get("/hello", (req, res) => {
-    var { access_token } = req.session.grant.response;
-    console.log(access_token);
-    res.end("nice!");
+    // var { access_token } = req.session.grant.response;
+    // console.log(access_token);
+    let message = "default";
+    if (req.session.grant) {
+      message = JSON.stringify(req.session.grant);
+    }
+    // var grantreq = JSON.stringify(req.session.grant);
+
+    res.end(`
+      <!DOCTYPE html>
+        <a href="/">Home</a>
+        <div style="width: 100%;
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 1140px;">${message}
+        </div>
+
+    `);
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
